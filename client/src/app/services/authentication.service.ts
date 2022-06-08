ae4b6741c3832771e3b674b7ffcf24819447d929
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-08 16:32:50 +0300

    add sign in functionality to auth service

--- client/src/app/services/authentication.service.ts
+++ client/src/app/services/authentication.service.ts
@@ -1,9 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  UserCredential,
+} from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(private http: HttpClient) {}
+  constructor(private auth: Auth) {}
+
+  signIn(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
 }
