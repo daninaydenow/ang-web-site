11e67a5527c4829cc17384d1fbfad4a0576d952d
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-08 16:43:24 +0300

    add comments

--- client/src/app/components/sign-in/sign-in.component.ts
+++ client/src/app/components/sign-in/sign-in.component.ts
@@ -10,17 +10,20 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
-  constructor(private atuhService: AuthenticationService) {
+  constructor(private authService: AuthenticationService) {
+    // clear local storage on initialization
     localStorage.clear();
   }
 
   ngOnInit(): void {}
 
+  // create form model with needed controls
   form = new FormGroup({
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
   });
 
+  // error extraction method for email control
   getEmailError(): string {
     if (this.form.get('email')?.hasError('required')) {
       return 'You must enter an email';
@@ -29,6 +32,7 @@ export class SignInComponent implements OnInit {
     return this.form.get('email')?.hasError('email') ? 'Not a valid email' : '';
   }
 
+  // error extraction method for password control
   getPasswordError(): string {
     if (this.form.get('password')?.hasError('required')) {
       return 'You must enter a password';
@@ -41,27 +45,52 @@ export class SignInComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid) {
-      this.atuhService
-        .signIn(this.form.get('email')?.value, this.form.get('password')?.value)
-        .then((userCredentials: UserCredential) => {
-          userCredentials.user
-            .getIdToken(true)
-            .then((token) => {
-              localStorage.setItem(
-                'user',
-                JSON.stringify({
-                  email: userCredentials.user.email,
-                  token: token,
-                })
-              );
-            })
-            .catch((err) => {
-              alert(err);
-            });
-        })
-        .catch((err) => {
-          alert(err.message);
-        });
+      this.singIn();
+      // PROMISE.THEN VARIANT
+      //
+      //
+      // this.authService
+      //   .signIn(this.form.get('email')?.value, this.form.get('password')?.value)
+      //   .then((userCredentials: UserCredential) => {
+      //     userCredentials.user
+      //       .getIdToken(true)
+      //       .then((token) => {
+      //         localStorage.setItem(
+      //           'user',
+      //           JSON.stringify({
+      //             email: userCredentials.user.email,
+      //             token: token,
+      //           })
+      //         );
+      //       })
+      //       .catch((err) => {
+      //         alert(err);
+      //       });
+      //   })
+      //   .catch((err) => {
+      //     alert(err.message);
+      //   });
+    }
+  }
+
+  async singIn() {
+    try {
+      // get user credentials
+      const userCredentials: UserCredential = await this.authService.signIn(
+        this.form.get('email')?.value,
+        this.form.get('password')?.value
+      );
+      // extract token from credentials
+      const token = await userCredentials.user.getIdToken(false);
+      // create user object
+      const user = {
+        email: userCredentials.user.email,
+        token,
+      };
+      // save user object in local storage
+      localStorage.setItem('user', JSON.stringify(user));
+    } catch (error) {
+      alert(error);
     }
   }
 }
