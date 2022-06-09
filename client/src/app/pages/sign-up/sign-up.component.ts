5703d85cec30cac3d4ad1f86a1ba5752e42cf6c0
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-09 10:27:31 +0300

    init sign up form

--- client/src/app/pages/sign-up/sign-up.component.ts
+++ client/src/app/pages/sign-up/sign-up.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    localStorage.clear();
   }
 
+  form = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  ngOnInit(): void {}
+
+  onSubmit() {}
+
+  getEmailError() {}
+
+  getPasswordError() {}
 }
