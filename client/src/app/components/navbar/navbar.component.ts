383ed96f2730980403f68a1ac1bde7095c20577b
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-09 13:22:02 +0300

    add basic user extraction for conditional nav renders

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user: string | null;
+  constructor() {
+    this.user = localStorage.getItem('user');
   }
 
+  ngOnInit(): void {
+    if (this.user) {
+      this.user = JSON.parse(this.user);
+    }
+  }
 }
