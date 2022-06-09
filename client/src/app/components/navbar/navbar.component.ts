32da9643540689310cfe835d798e4d66b0bf8cee
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-09 14:23:17 +0300

    Update navbar.component.ts

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  user: string | null;
+  user;
   constructor() {
     this.user = localStorage.getItem('user');
   }
