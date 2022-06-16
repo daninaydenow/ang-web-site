06259be5df0fc0bf94e6ec92f5f5e68d3d454d68
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 13:52:48 +0300

    add sign out path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignOutComponent } from './pages/sign-out/sign-out.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: 'sign-in',
     component: SignInComponent,
+  },
+  {
+    path: 'sign-out',
+    component: SignOutComponent,
   },
   {
     path: 'sign-up',
