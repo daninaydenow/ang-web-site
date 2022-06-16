1983a03c71e59d7045ea7b622941b2d858f14a56
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 13:42:40 +0300

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'sign-up',
     component: SignUpComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
 ];
 
