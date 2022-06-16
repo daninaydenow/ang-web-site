9ec541804efd26191a4b6f9c12236cd9dc2ba5fe
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 16:47:53 +0300

    Update home.component.ts

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -19,6 +19,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.authService.getLocalStorageUser();
-    this.userName = this.user.email?.split('@')[0];
+    this.userName = this.user?.email?.split('@')[0];
   }
 }
