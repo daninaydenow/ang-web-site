e9eb0cc4235be916ace5fb6a775253fb7eb844c2
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 13:56:49 +0300

    add sign out logic

--- client/src/app/pages/sign-out/sign-out.component.ts
+++ client/src/app/pages/sign-out/sign-out.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-sign-out',
   templateUrl: './sign-out.component.html',
-  styleUrls: ['./sign-out.component.css']
+  styleUrls: ['./sign-out.component.css'],
 })
 export class SignOutComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.authService
+      .signOut()
+      .then(() => {
+        localStorage.clear();
+        this.router.navigate(['/sign-in']);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-
 }
