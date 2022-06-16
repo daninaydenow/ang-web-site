3afa5d82666b15c940e657cf279ef9c10cdeb297
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 16:50:58 +0300

    get all movies from api

--- client/src/app/services/movie.service.ts
+++ client/src/app/services/movie.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  private baseUrl: string = 'https://movies-app1.p.rapidapi.com/api';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll() {
+    const options = {
+      headers: {
+        'X-RapidAPI-Key': '58b017252fmsh8384cd09f5f34bbp16a752jsn216133d0bee5',
+        'X-RapidAPI-Host': 'movies-app1.p.rapidapi.com',
+      },
+    };
+    return this.http.get(`${this.baseUrl}/movies`, options);
+  }
 }
