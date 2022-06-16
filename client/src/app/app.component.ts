7e9a9f5e8bb5a5545159d819f6a11dbd6a632b30
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 13:42:32 +0300

    add auth service as a provider for children

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [AuthenticationService],
 })
 export class AppComponent {
   title = 'client';
