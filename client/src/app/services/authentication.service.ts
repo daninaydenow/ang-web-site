1272a957d0ac6742bfc2b7021fb1354a38f51e51
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-16 13:37:30 +0300

    make user as an observable and stream user info

--- client/src/app/services/authentication.service.ts
+++ client/src/app/services/authentication.service.ts
@@ -5,12 +5,20 @@ import {
   UserCredential,
 } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
+  private userSource = new Subject<User>();
+  user$ = this.userSource.asObservable();
   constructor(private auth: Auth) {}
+
+  getLocalStorageUser() {
+    this.userSource.next(JSON.parse(localStorage.getItem('user')!));
+  }
 
   signIn(email: string, password: string): Promise<UserCredential> {
     return signInWithEmailAndPassword(this.auth, email, password);
