1e3cb58a41aa5474f46575f5ccdc103eb6eb8141
Daniel Naydenov <daninaydenow@gmail.com>
2022-06-26 19:03:28 +0300

    Add unsubscribe upon comp destruction

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/User';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { MovieService } from 'src/app/services/movie.service';
@@ -8,15 +9,16 @@ import { MovieService } from 'src/app/services/movie.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   user!: User;
   userName: string | undefined = '';
   movies: any = [];
+  subscribtion!: Subscription;
   constructor(
     private authService: AuthenticationService,
     private movieService: MovieService
   ) {
-    this.authService.user$.subscribe((user) => {
+    this.subscribtion = this.authService.user$.subscribe((user) => {
       this.user = user;
     });
   }
@@ -26,5 +28,9 @@ export class HomeComponent implements OnInit {
     this.movieService.getAll().subscribe((response) => {
       this.movies.push(response);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
   }
 }
